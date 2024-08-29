29bc7866b32082c9088fb0cc7f0968c47e60ec21
Slava Maslov <viacheslav.maslov@esrlabs.com>
2024-08-29 16:49:12 +0200

    Refactor sema_test.sh script for improved readability and exit code management
    - Introduced named exit codes (`EXIT_SUCCESS`, `EXIT_SEMA_TAKEN_BY_OTHER`, `EXIT_SEMA_TAKEN_BY_USER`) for better clarity.
    - Added argument parsing for a verbose mode using `-v` option.
    - Simplified file existence check with more concise code.
    - Streamlined conditional checks for semaphore status.
    - Replaced multiple `if` blocks with shorter, more readable expressions.

--- sema_test.sh
+++ sema_test.sh
@@ -2,62 +2,68 @@
 
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-# Path to the file containing the release time
-FILE_PATH=$SCRIPT_DIR/sema
-# Check if the file exists
-if [ ! -f "$FILE_PATH" ]; then
-    echo "Semaphore is not taken"
-    touch $FILE_PATH
-    chown $USER:elite $FILE_PATH
-    exit 0
-fi
+FILE_PATH="$SCRIPT_DIR/sema"
 
+# Enum for exit codes
+EXIT_SUCCESS=0
+EXIT_SEMA_TAKEN_BY_OTHER=255
+EXIT_SEMA_TAKEN_BY_USER=1
+
+# Argument parsing
+SEMA_VERBOSE=0
+while getopts ":v" opt; do
+    case $opt in
+        v)
+            SEMA_VERBOSE=1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit $EXIT_SUCCESS
+            ;;
+    esac
+done
+
+# Determine if the script is running in an interactive shell
 if [ -t 0 ]; then
-    #echo "interactive"
     SEMA_VERBOSE=1
-else
-    #echo "non-interactive"
-    SEMA_VERBOSE=0
 fi
 
-# Convert release time and current time to seconds since the epoch for comparison
-CURRENT_TIME_SEC=$(date +%s)
+# Check if the semaphore file exists
+if [ ! -f "$FILE_PATH" ]; then
+    [ "$SEMA_VERBOSE" -ne 0 ] && echo "Semaphore is not taken"
+    #touch "$FILE_PATH"
+    #chown "$USER:elite" "$FILE_PATH"
+    exit $EXIT_SUCCESS
+fi
 
-# Get the current system username
+CURRENT_TIME_SEC=$(date +%s)
 CURRENT_USERNAME=$USER
 CURRENT_USER_IP=$(echo $SSH_CLIENT | awk '{ print $1}')
 
-SEMA_USER=$(sed -n '1p' $FILE_PATH)
-SEMA_CURRENT_DATE=$(sed -n '2p' $FILE_PATH)
-SEMA_CURRENT_TIME_IN_SEC=$(sed -n '3p' $FILE_PATH)
-SEMA_RELEASE_DATE=$(sed -n '4p' $FILE_PATH)
-SEMA_RELEASE_TIME_IN_SEC=$(sed -n '5p' $FILE_PATH)
-SEMA_USER_IP=$(sed -n '6p' $FILE_PATH)
-SEMA_AT_JOB_NUMBER=$(sed -n '7p' $FILE_PATH)
-
+# Read semaphore details from the file
+SEMA_USER=$(sed -n '1p' "$FILE_PATH")
+SEMA_CURRENT_DATE=$(sed -n '2p' "$FILE_PATH")
+SEMA_CURRENT_TIME_IN_SEC=$(sed -n '3p' "$FILE_PATH")
+SEMA_RELEASE_DATE=$(sed -n '4p' "$FILE_PATH")
+SEMA_RELEASE_TIME_IN_SEC=$(sed -n '5p' "$FILE_PATH")
+SEMA_USER_IP=$(sed -n '6p' "$FILE_PATH")
+SEMA_AT_JOB_NUMBER=$(sed -n '7p' "$FILE_PATH")
 
 # Compare the username from the file with the current system username
 if [ "$SEMA_USER" != "$CURRENT_USERNAME" ]; then
     if [ "$SEMA_RELEASE_TIME_IN_SEC" -le "$CURRENT_TIME_SEC" ]; then
-        if [ "$SEMA_VERBOSE" != 0 ]; then
-            echo "Semaphore is not taken"
-        fi
-        exit 0 # semaphore is not taken
+        [ "$SEMA_VERBOSE" -ne 0 ] && echo "Semaphore is not taken"
+        exit $EXIT_SUCCESS # Semaphore is not taken
     else
         echo "$SEMA_USER semaphore is active until $SEMA_RELEASE_DATE"
-        exit 255 # semaphore is taken by another USER
+        exit $EXIT_SEMA_TAKEN_BY_OTHER # Semaphore is taken by another user
     fi
 else
     if [ "$SEMA_RELEASE_TIME_IN_SEC" -le "$CURRENT_TIME_SEC" ]; then
-        if [ "$SEMA_VERBOSE" != 0 ]; then
-            echo "Your semaphore is released"
-        fi
-        exit 0 # semaphore is not taken
+        [ "$SEMA_VERBOSE" -ne 0 ] && echo "Your semaphore is released"
+        exit $EXIT_SUCCESS # Semaphore is not taken
     else
-        if [ "$SEMA_VERBOSE" != 0 ]; then
-            echo "Your semaphore is still active until $SEMA_RELEASE_DATE $SEMA_VERBOSE"
-        fi
-        exit 1 # semaphore is taken by the USER
+        [ "$SEMA_VERBOSE" -ne 0 ] && echo "Your semaphore is still active until $SEMA_RELEASE_DATE"
+        exit $EXIT_SEMA_TAKEN_BY_USER # Semaphore is taken by the user
     fi
 fi
-
